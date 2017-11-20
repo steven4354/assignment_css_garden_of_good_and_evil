a3b83e8ac21953a1614c32388c8fda368bfa9723
Steven Li <stevenli@Stevens-MacBook-Pro.local>
2017-11-20 15:58:53 -0800

    finsihed

--- routes/garden.js
+++ routes/garden.js
@@ -9,7 +9,10 @@ const router = Express.Router();
 // let backgroundClass = "";
 
 router.get("/", (req, res) => {
-  res.render("garden", {});
+  res.render("garden", {
+    goodEvil: req.cookies.goodevil,
+    backgroundClass: req.cookies.goodevil,
+  });
 });
 
 router.get("/responses", (req, res) => {
@@ -17,11 +20,37 @@ router.get("/responses", (req, res) => {
   if (req.cookies.goodevil === "good") {
     job1 = "Angel";
     job2 = "Luke Skywalker";
+    image = "/images/luke.jpg"
   } else if (req.cookies.goodevil === "evil") {
     job1 = "Devil";
     job2 = "Darth Vader";
+    image = "/images/darth.jpeg"
   }
+
+  let insanityimage;
+  switch(req.cookies.insanity){
+    case 1:
+      insanityimage = '/images/1.png';
+      break;
+    case 2:
+      insanityimage = '/images/2.png';
+      break;
+    case 3:
+      insanityimage = '/images/3.png';
+      break;
+    case 4:
+      insanityimage = '/images/4.png';
+      break;
+    case 5:
+      insanityimage = '/images/5.png';
+      break;
+    default:
+      insanityimage = '/images/1.png';
+      break;
+  }
+
   res.render("responses", {
+    image: image,
     goodEvil: req.cookies.goodevil,
     backgroundClass: req.cookies.goodevil,
     favoritefood: req.cookies.favoritefood,
@@ -30,8 +59,12 @@ router.get("/responses", (req, res) => {
     worstfood: req.cookies.worstfood,
     worstcolor: req.cookies.worstcolor,
     worstanimal: req.cookies.worstanimal,
+    textcolor1: req.cookies.favoritecolor,
+    textcolor2: req.cookies.worstcolor,
+    insanityimage: insanityimage,
     job1: job1,
-    job2: job2
+    job2: job2,
+    bio: req.cookies.bio
   });
 });
 
