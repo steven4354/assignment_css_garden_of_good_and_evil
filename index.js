288d9b178d66663cf041de3e10a931e9e2163013
Austin1780 <austinwilliamsmith@gmail.com>
2017-11-20 11:33:17 -0700

    added HTML elements

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const Express = require("express");
 const hb = require("express-handlebars");
+const router = Express.Router();
 const garden = require("./routes/garden");
 
 const port = 3000;
 const host = "localhost";
 
-const app = Express()
+const app = Express();
 
 const hbs = hb.create({
   partialsDir: "views/",
--- routes/garden.js
+++ routes/garden.js
@@ -4,8 +4,8 @@ const fs = require("fs");
 
 //const garden = require('./views/garden.handlebars')
 
-router.get("/", (res, req) => {
-  res.render('garden')
-})
+router.get("/", (req, res) => {
+  res.render("garden", { gardens: "" });
+});
 
 module.exports = router;
